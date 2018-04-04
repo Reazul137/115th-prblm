cd5b0cdac014f0a0de4fc528672bda8bff362078
Reazul Islam <reazulislam137@gmail.com>
2018-04-04 16:27:56 +0600

    first commit

---
+++ april-04-04.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "\n\n Print the result of some specific operation : \n";
+    cout << "---------------------------------------------------\n";
+
+    cout << " Result of 1st expression is : " << ( -2 +4 *6 ) << "\n";
+    cout << " Result of 2nd expression is : " << ((35 +5) %7) << "\n";
+    cout << " Result of 3rd expression is : " << (14+-4*6/11) << "\n";
+    cout << " Result of 4th expression is : " << (2 + 15 / 6*1 - 7%2) << "\n";
+
+    return 0;
+}
